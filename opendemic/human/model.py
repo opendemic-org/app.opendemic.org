fde0c96e8f6c858edd58d65987d98a8ee4a98882
David Hachuel <david@auggi.ai>
2020-04-19 18:10:09 -0400

    refactored human model

--- opendemic/human/model.py
+++ opendemic/human/model.py
@@ -95,47 +95,6 @@ class Human(object):
 		else:
 			return None
 
-	@staticmethod
-	def human_exists(human_id: str) -> bool:
-		# validate format
-		if not verify_uuid_regex(s=human_id):
-			return False
-
-		# validate `human_id` existence
-		rdb = RDBManager()
-		human_id_search_results, _ = rdb.execute(
-			sql_query="""
-						SELECT *
-						FROM `humans`
-						WHERE `id` = '{}'
-					""".format(human_id)
-		)
-
-		# return results
-		return len(human_id_search_results) == 1
-
-	@staticmethod
-	def validate_human_id(human_id: str) -> (bool, dict):
-		# validate format
-		if not verify_uuid_regex(s=human_id):
-			return False
-
-		# validate `human_id` existence
-		rdb = RDBManager()
-		human_id_search_results, _ = rdb.execute(
-			sql_query="""
-					SELECT *
-					FROM `humans`
-					WHERE `id` = '{}'
-				""".format(human_id)
-		)
-
-		# return results
-		if len(human_id_search_results) == 1:
-			return True, human_id_search_results[0]
-		else:
-			return False, None
-
 	def log_symptom(self, symptom_name: str):
 		# validate inputs
 		if not Symptoms.has_value(symptom_name):
@@ -182,111 +141,6 @@ class Human(object):
 			return float(most_recent_location[0]['latitude']), float(most_recent_location[0]['longitude'])
 		else:
 			return None
-
-	@staticmethod
-	def get_risky_humans(lat: float, lng: float, days_window: int, km_radius: int = None):
-		# get DB
-		rdb = RDBManager()
-
-		# fetch cases
-		sql_query = """						
-				SELECT 
-					CASE
-						WHEN agg.`risk_level` = 5 THEN round(agg.`latitude` + gauss(0,0.002), 5)
-						ELSE round(agg.`latitude` + gauss(0,0.001), 5)
-					 END AS 'latitude',
-					 CASE
-						WHEN agg.`risk_level` = 5 THEN round(agg.`longitude` + gauss(0,0.002), 5)
-						ELSE round(agg.`longitude` + gauss(0,0.001), 5)
-					 END AS 'longitude',
-					CASE
-						WHEN agg.`risk_level` > 1 THEN agg.`risk_level`
-						ELSE COUNT(DISTINCT(agg.`symptom`))
-					END AS 'mag' 
-				FROM (
-				SELECT
-					geo.`human_id`,
-					DATE(sym.`created`) AS 'date',
-					geo.`latitude`,
-					geo.`longitude`,
-					sym.`symptom`,
-					round(( 6373 * acos( least(1.0,  
-						cos( radians({}) ) 
-						* cos( radians(geo.`latitude`) ) 
-						* cos( radians(geo.`longitude`) - radians({}) ) 
-						+ sin( radians({}) ) 
-						* sin( radians(geo.`latitude`) 
-					  ) ) ) 
-					), 1) as 'distance',
-					CASE 
-						WHEN sym.`symptom` = 'verified_covid19' THEN 5
-						WHEN sym.`symptom` = 'confirmed_covid19' THEN 4
-						ELSE 1
-					END AS 'risk_level'
-				FROM `geolocations` as geo
-				LEFT JOIN `symptoms` as sym
-				ON 
-					geo.`human_id` = sym.`human_id`
-					AND 
-					DATE(sym.`created`) = DATE(geo.`created`)
-				WHERE 
-					sym.`symptom` is not null
-					AND
-					geo.`latitude` is not null
-					AND
-					geo.`longitude` is not null
-					AND
-					sym.`created` >= DATE(NOW()) - INTERVAL {} DAY
-					AND
-					(sym.`value` is not null or sym.`value` > 0)
-				{}
-				) as agg
-				GROUP BY 
-					agg.`human_id`,
-					agg.`date`,
-					agg.`latitude`,
-					agg.`longitude`,
-					agg.`risk_level`;
-									""".format(
-			mysql_db_format_value(value=lat),
-			mysql_db_format_value(value=lng),
-			mysql_db_format_value(value=lat),
-			days_window,
-			"HAVING distance <= {}".format(km_radius*5) if km_radius is not None else ""
-		)
-		risky_humans, _ = rdb.execute(sql_query=sql_query)
-		return risky_humans
-
-	@staticmethod
-	def get_proximity_alert(lat: float, lng: float):
-		km_radius = int(CONFIG.get('km_radius'))
-		days_window = int(CONFIG.get('days_window'))
-		risky_humans = Human.get_risky_humans(lat=lat, lng=lng, days_window=days_window, km_radius=km_radius)
-
-		# create alert message
-		if len(risky_humans) == 0:
-			alert_emoji = '🔔'
-			alert_info = "*0 individuals* who self-reported symptoms (fever, cough, shortness of breath) were found"
-		else:
-			alert_emoji = '❗'
-			alert_info = "*{} individual(s)* who self-reported symptoms (fever, cough, shortness of breath) were found. ".format(
-				len(risky_humans)
-			)
-			alert_info += "\n\nSee specific locations on the map below ⬇️ and *please exercise additional caution* ⚠️."
-
-		alert_message = """
-		{} During the last {} days and within a {} km ({} miles) radius from your current location, {}
-
-		DISCLAIMER: This only represents the data *Opendemic* users have shared and might not be complete. Always be cautious and follow your local public health authority's guidelines.
-				""".format(
-			alert_emoji,
-			days_window,
-			km_radius,
-			round(km_radius / 1.609, 1),
-			alert_info
-		)
-
-		return alert_message
 
 	def send_proximity_alert(self, lat: float, lng: float):
 		# verify user has telegram id
@@ -434,6 +288,111 @@ class Human(object):
 		return None
 
 	@staticmethod
+	def get_risky_humans(lat: float, lng: float, days_window: int, km_radius: int = None):
+		# get DB
+		rdb = RDBManager()
+
+		# fetch cases
+		sql_query = """						
+				SELECT 
+					CASE
+						WHEN agg.`risk_level` = 5 THEN round(agg.`latitude` + gauss(0,0.002), 5)
+						ELSE round(agg.`latitude` + gauss(0,0.001), 5)
+					 END AS 'latitude',
+					 CASE
+						WHEN agg.`risk_level` = 5 THEN round(agg.`longitude` + gauss(0,0.002), 5)
+						ELSE round(agg.`longitude` + gauss(0,0.001), 5)
+					 END AS 'longitude',
+					CASE
+						WHEN agg.`risk_level` > 1 THEN agg.`risk_level`
+						ELSE COUNT(DISTINCT(agg.`symptom`))
+					END AS 'mag' 
+				FROM (
+				SELECT
+					geo.`human_id`,
+					DATE(sym.`created`) AS 'date',
+					geo.`latitude`,
+					geo.`longitude`,
+					sym.`symptom`,
+					round(( 6373 * acos( least(1.0,  
+						cos( radians({}) ) 
+						* cos( radians(geo.`latitude`) ) 
+						* cos( radians(geo.`longitude`) - radians({}) ) 
+						+ sin( radians({}) ) 
+						* sin( radians(geo.`latitude`) 
+					  ) ) ) 
+					), 1) as 'distance',
+					CASE 
+						WHEN sym.`symptom` = 'verified_covid19' THEN 5
+						WHEN sym.`symptom` = 'confirmed_covid19' THEN 4
+						ELSE 1
+					END AS 'risk_level'
+				FROM `geolocations` as geo
+				LEFT JOIN `symptoms` as sym
+				ON 
+					geo.`human_id` = sym.`human_id`
+					AND 
+					DATE(sym.`created`) = DATE(geo.`created`)
+				WHERE 
+					sym.`symptom` is not null
+					AND
+					geo.`latitude` is not null
+					AND
+					geo.`longitude` is not null
+					AND
+					sym.`created` >= DATE(NOW()) - INTERVAL {} DAY
+					AND
+					(sym.`value` is not null or sym.`value` > 0)
+				{}
+				) as agg
+				GROUP BY 
+					agg.`human_id`,
+					agg.`date`,
+					agg.`latitude`,
+					agg.`longitude`,
+					agg.`risk_level`;
+									""".format(
+			mysql_db_format_value(value=lat),
+			mysql_db_format_value(value=lng),
+			mysql_db_format_value(value=lat),
+			days_window,
+			"HAVING distance <= {}".format(km_radius*5) if km_radius is not None else ""
+		)
+		risky_humans, _ = rdb.execute(sql_query=sql_query)
+		return risky_humans
+
+	@staticmethod
+	def get_proximity_alert(lat: float, lng: float):
+		km_radius = int(CONFIG.get('km_radius'))
+		days_window = int(CONFIG.get('days_window'))
+		risky_humans = Human.get_risky_humans(lat=lat, lng=lng, days_window=days_window, km_radius=km_radius)
+
+		# create alert message
+		if len(risky_humans) == 0:
+			alert_emoji = '🔔'
+			alert_info = "*0 individuals* who self-reported symptoms (fever, cough, shortness of breath) were found"
+		else:
+			alert_emoji = '❗'
+			alert_info = "*{} individual(s)* who self-reported symptoms (fever, cough, shortness of breath) were found. ".format(
+				len(risky_humans)
+			)
+			alert_info += "\n\nSee specific locations on the map below ⬇️ and *please exercise additional caution* ⚠️."
+
+		alert_message = """
+		{} During the last {} days and within a {} km ({} miles) radius from your current location, {}
+
+		DISCLAIMER: This only represents the data *Opendemic* users have shared and might not be complete. Always be cautious and follow your local public health authority's guidelines.
+				""".format(
+			alert_emoji,
+			days_window,
+			km_radius,
+			round(km_radius / 1.609, 1),
+			alert_info
+		)
+
+		return alert_message
+
+	@staticmethod
 	def telegram_id_exists(telegram_human_id: int):
 		rdb = RDBManager()
 		_, records = rdb.execute(
@@ -522,6 +481,47 @@ class Human(object):
 		# return results
 		if len(telegram_human_id_search_results) == 1:
 			return True, telegram_human_id_search_results[0]['id']
+		else:
+			return False, None
+
+	@staticmethod
+	def human_exists(human_id: str) -> bool:
+		# validate format
+		if not verify_uuid_regex(s=human_id):
+			return False
+
+		# validate `human_id` existence
+		rdb = RDBManager()
+		human_id_search_results, _ = rdb.execute(
+			sql_query="""
+						SELECT *
+						FROM `humans`
+						WHERE `id` = '{}'
+					""".format(human_id)
+		)
+
+		# return results
+		return len(human_id_search_results) == 1
+
+	@staticmethod
+	def validate_human_id(human_id: str) -> (bool, dict):
+		# validate format
+		if not verify_uuid_regex(s=human_id):
+			return False
+
+		# validate `human_id` existence
+		rdb = RDBManager()
+		human_id_search_results, _ = rdb.execute(
+			sql_query="""
+					SELECT *
+					FROM `humans`
+					WHERE `id` = '{}'
+				""".format(human_id)
+		)
+
+		# return results
+		if len(human_id_search_results) == 1:
+			return True, human_id_search_results[0]
 		else:
 			return False, None
 
